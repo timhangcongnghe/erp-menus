489d3bc46923e63eb8d0ef709533a29be5071117
Luan Pham <luanpm88@gmail.com>
2017-04-05 00:37:21 -0700

    fix menu children order

--- app/models/erp/menus/menu.rb
+++ app/models/erp/menus/menu.rb
@@ -1,43 +1,43 @@
 module Erp::Menus
   class Menu < ApplicationRecord
-		include Erp::CustomOrder		
+		include Erp::CustomOrder
 		mount_uploader :image_url_1, Erp::Menus::MenuImageUploader
 		mount_uploader :image_url_2, Erp::Menus::MenuImageUploader
 		mount_uploader :menu_icon, Erp::Menus::MenuImageUploader
-		
+
     validates :name, :presence => true
     belongs_to :creator, class_name: "Erp::User"
     belongs_to :parent, class_name: "Erp::Menus::Menu", optional: true
-    has_many :children, class_name: "Erp::Menus::Menu", foreign_key: "parent_id"
+    has_many :children, -> { order(custom_order: :asc) }, class_name: "Erp::Menus::Menu", foreign_key: "parent_id"
     has_many :related_menus, foreign_key: "parent_id", inverse_of: :parent, dependent: :destroy
     accepts_nested_attributes_for :related_menus, :reject_if => lambda { |a| a[:menu_id].blank? }, :allow_destroy => true
-    
+
     if Erp::Core.available?("products")
 			has_and_belongs_to_many :categories, class_name: "Erp::Products::Category"
 			belongs_to :brand_group, class_name: "Erp::Products::BrandGroup"
 			belongs_to :brand, class_name: "Erp::Products::Brand", foreign_key: "brand_id"
-			
+
 			# display brand group name
 			def brand_group_name
 				brand_group.present? ? brand_group.name : ''
 			end
-			
+
 			def get_brand_groups
 				brand_group.brand_group_details
 			end
-			
+
 			# display brand name
 			def brand_name
 				brand.present? ? brand.name : ''
 			end
 		end
-    
+
     STYLE_COLOR_1 = 'supper1'
     STYLE_COLOR_2 = 'supper2'
     STYLE_COLOR_3 = 'supper3'
-    
+
     after_save :update_level
-    
+
     def self.get_style_color_options()
       [
 				{text: '', value: ''},
@@ -46,32 +46,32 @@ module Erp::Menus
         {text: I18n.t('erp_menus_menus.fruit_salad'), value: STYLE_COLOR_3}
       ]
     end
-    
+
     def self.get_active
 			self.where(archived: false).order("custom_order ASC")
 		end
-    
+
     def self.is_hot
 			self.where(is_hot: true)
 		end
-    
+
     def self.get_hot_menus
 			self.get_active.where(is_hot: true)
 			               .where(parent_id: nil)
 		end
-    
+
     def self.get_menus
 			self.get_active.where(parent_id: nil)
 		end
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
       and_conds = []
-      
+
       # show archived items condition - default: false
 			show_archived = false
-			
+
 			#filters
 			if params["filters"].present?
 				params["filters"].each do |ft|
@@ -88,7 +88,7 @@ module Erp::Menus
 					and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
 				end
 			end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -99,76 +99,76 @@ module Erp::Menus
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # join with parent menu for search menu
       query = query.joins("LEFT JOIN erp_menus_menus parents_erp_menus_menus ON parents_erp_menus_menus.id = erp_menus_menus.parent_id")
-      
+
       # showing archived items if show_archived is not true
 			query = query.where(archived: false) if show_archived == false
 
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
         order += " #{params[:sort_direction]}" if params[:sort_direction].present?
-        
+
         query = query.order(order)
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='')
       query = self.all
-      
+
       if keyword.present?
         keyword = keyword.strip.downcase
         query = query.where('LOWER(name) LIKE ?', "%#{keyword}%")
       end
-      
+
       query = query.limit(8).map{|menu| {value: menu.id, text: menu.name} }
     end
-    
+
     def archive
 			update_columns(archived: true)
 		end
-		
+
 		def unarchive
 			update_columns(archived: false)
 		end
-    
+
     def self.archive_all
 			update_all(archived: true)
 		end
-    
+
     def self.unarchive_all
 			update_all(archived: false)
 		end
-    
+
     # display name
     def parent_name
 			parent.present? ? parent.name : ''
-		end      
-    
+		end
+
     # get self and children ids
     def get_self_and_children_ids
       ids = [self.id]
       ids += get_children_ids_recursive
       return ids
 		end
-    
+
     # get children ids recursive
     def get_children_ids_recursive
       ids = []
@@ -180,11 +180,11 @@ module Erp::Menus
 			end
       return ids
 		end
-    
+
     def get_same_level_menus
 			return self.parent.nil? ? Menu.where(level: 1) : self.parent.children
 		end
-    
+
     def get_related_sidebar_menus
 			if self.children.empty?
 				{
@@ -198,14 +198,14 @@ module Erp::Menus
 				}
 			end
 		end
-    
+
     if Erp::Core.available?("products")
 			def get_products_for_categories(params)
 				records = Erp::Products::Product.get_active
-													.where(category_id: self.get_all_related_category_ids)				
+													.where(category_id: self.get_all_related_category_ids)
 				return records
 			end
-			
+
 			def get_all_related_category_ids
 				category_ids = []
 				menu_ids = self.get_self_and_children_ids
@@ -218,12 +218,12 @@ module Erp::Menus
 				return category_ids.uniq
 			end
 		end
-    
+
     # init custom order
     def init_custom_order
 			self.update_column(:custom_order, self.class.maximum("custom_order").to_i + 1)
 		end
-    
+
     # update level
     def update_level
 			level = 1
@@ -232,10 +232,10 @@ module Erp::Menus
 				level += 1
 				parent = parent.parent
 			end
-			
+
 			level
 		end
-    
+
     # get self parent menus
     def self_and_parent_menus
 			arr = [self]
@@ -244,7 +244,7 @@ module Erp::Menus
 				arr << father
 				father = father.parent
 			end
-			
+
 			return arr.reverse
 		end
   end
