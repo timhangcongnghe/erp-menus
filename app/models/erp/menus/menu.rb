cb3cb28f8b23b8e771bc155bd6ca6ec8ac6c060c
Luan Pham <luanpm88@gmail.com>
2017-05-05 01:14:39 -0700

    sort menu page

--- app/models/erp/menus/menu.rb
+++ app/models/erp/menus/menu.rb
@@ -217,17 +217,23 @@ module Erp::Menus
 				if params[:brand_ids].present?
 					records = records.where(brand_id: params[:brand_ids])
 				end
-				
+
 				if params[:is_business_choises].present?
 					records = records.where(is_business_choices: true)
 				end
-				
+
 				if params[:is_deal].present?
 					records = records.where(is_deal: true)
 				end
-				
+
 				if params[:is_bestseller].present?
 					records = records.where(is_bestseller: true)
+				end
+
+				if params[:sort_by].present?
+					records = records.order(params[:sort_by].gsub('_', ' '))
+				else
+					records = records.order("created_at DESC")
 				end
 
 				return records
