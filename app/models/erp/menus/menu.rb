23c101110fcbc7f6700c3ae2cc2226fcb9243fb1
Luan Pham <luanpm88@gmail.com>
2017-05-19 00:59:07 -0700

    fix filter missing

--- app/models/erp/menus/menu.rb
+++ app/models/erp/menus/menu.rb
@@ -214,6 +214,32 @@ module Erp::Menus
 					records = records.where(brand_id: self.brand_id)
 				end
 
+				if params[:brand_ids].present?
+					records = records.where(brand_id: params[:brand_ids])
+				end
+
+				if params[:brand_id].present?
+					records = records.where(brand_id: params[:brand_id])
+				end
+
+				if params[:is_business_choises].present?
+					records = records.where(is_business_choices: true)
+				end
+
+				if params[:is_deal].present?
+					records = records.where(is_deal: true)
+				end
+
+				if params[:is_bestseller].present?
+					records = records.where(is_bestseller: true)
+				end
+
+				if params[:sort_by].present?
+					records = records.order(params[:sort_by].gsub('_', ' '))
+				else
+					records = records.order("created_at DESC")
+				end
+
 				return records
 			end
 
