3f495f2e245b94ae7a41b1f29795c0979cf45bb0
Luan Pham <luanpm88@gmail.com>
2017-07-03 01:05:10 -0700

    clean up

---
+++ app/decorators/models/product_decorator.rb
@@ -0,0 +1,16 @@
+Erp::Products::Product.class_eval do
+  # Find all menus of a product
+	def find_menus
+		self.category.nil? ? nil : self.category.menus
+	end
+
+	def find_menu
+		all_menus = self.find_menus
+		if self.brand_id.present?
+			menus = all_menus.where(brand_id: self.brand_id)
+		end
+		menus = all_menus if menus.empty?
+
+		menus.last
+	end
+end
